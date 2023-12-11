127ca2d8344fafa8ef21167303af6c442271cfb8
Junior-Stranner <Ju-stranner@outlook.com>
2023-12-11 11:53:16 -0300

    Inicializing my Virtual-Store

---
+++ Backend/lojavirtual/src/main/java/br/com/jujubaprojects/lojavirtual/Controller/Hello.java
@@ -0,0 +1,17 @@
+package br.com.jujubaprojects.lojavirtual.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/hello/{nome}")
+    public String hello(@PathVariable("nome") String nome){
+        return "Hello "+nome;
+    }
+    
+}
---
+++ Backend/lojavirtual/src/test/java/br/com/jujubaprojects/lojavirtual/LojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
